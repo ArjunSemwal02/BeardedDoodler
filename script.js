7701aa5a5cbb3a0a775d521f04f6f9045398fc76
Arjun <arjunsemwal02@gmail.com>
2024-12-02 13:54:14 +0530

    added left movement control to doodler

--- script.js
+++ script.js
@@ -1,69 +1,6 @@
 const gridElement = document.querySelector('.grid');
 const beardedDoodlerElement = document.createElement('div')
 
-let doodlerLeftSpace
-let doodlerBottomSpace = 5
-let isGameOver = false
-let platformCount = 6
-let platforms = []
-let upTimerId
-let downTimerId
-
-const createDoodler = () => {
-    beardedDoodlerElement.classList.add('doodler')
-    gridElement.appendChild(beardedDoodlerElement)
-    doodlerLeftSpace = platforms[0].left
-
-    beardedDoodlerElement.style.left = doodlerLeftSpace + 'rem'
-    beardedDoodlerElement.style.bottom = doodlerBottomSpace + 'rem'
-}
-
-const movePlatforms = () => {
-    if(doodlerBottomSpace > 4){
-        platforms.forEach(platform => {
-            platform.bottom -= .5
-            let visual = platform.visual
-            visual.style.bottom = platform.bottom + 'rem'
-        })
-    }
-}
-
-const jump = () => {
-    clearInterval(downTimerId)
-    upTimerId = setInterval(() => {
-        doodlerBottomSpace += .5
-        beardedDoodlerElement.style.bottom = doodlerBottomSpace + 'rem'
-        if(doodlerBottomSpace > 20)
-            fall()
-    }, 30)
-}
-
-const fall = () => {
-    clearInterval(upTimerId)
-    downTimerId = setInterval(() => {
-        doodlerBottomSpace -= .5
-        beardedDoodlerElement.style.bottom = doodlerBottomSpace + 'rem'
-        if(doodlerBottomSpace <= 0)
-            gameOver()
-    }, 30)
-}
-
-function gameOver(){
-    console.log('Game is Over')
-    isGameOver = true
-    clearInterval(upTimerId)
-    clearInterval(downTimerId)
-}
-
-const start = () => {
-    if(!isGameOver){
-        createPlatforms()
-        createDoodler()
-        setInterval(movePlatforms, 30)
-        jump()
-    }
-}
-
 class Platform{
     constructor(newPlatformBottom){
         this.bottom = newPlatformBottom
@@ -78,15 +15,120 @@ class Platform{
     }
 }
 
+let doodlerLeftSpace
+let startPoint = 5
+let doodlerBottomSpace = startPoint
+let isGameOver = false
+let platformCount = 7
+let platforms = []
+let upTimerId
+let downTimerId
+let isJumping = false
+let isGoingLeft = false
+let isGoingRight = false
+let leftTimerId
+let rightTimerId
+
+const createDoodler = () => {
+    beardedDoodlerElement.classList.add('doodler')
+    gridElement.appendChild(beardedDoodlerElement)
+    doodlerLeftSpace = platforms[0].left
+    beardedDoodlerElement.style.left = doodlerLeftSpace + 'rem'
+    beardedDoodlerElement.style.bottom = doodlerBottomSpace + 'rem'
+}
+
 const createPlatforms = () => {
     for( let i = 0; i < platformCount; i++){
-       let platformGap = 57 / platformCount
+       let platformGap = 70 / platformCount
        let newPlatformBottom = 9 + i * platformGap
        let newPlaform = new Platform(newPlatformBottom)
        platforms.push(newPlaform)
        console.log(platforms)
     }
 }
+
+const movePlatforms = () => {
+    if(doodlerBottomSpace > 5){
+        platforms.forEach(platform => {
+            platform.bottom -= .3
+            let visual = platform.visual
+            visual.style.bottom = platform.bottom + 'rem'
+        })
+    }
+}
+
+const jump = () => {
+    clearInterval(downTimerId)
+    isJumping = true
+    upTimerId = setInterval(() => {
+        doodlerBottomSpace += .8
+        beardedDoodlerElement.style.bottom = doodlerBottomSpace + 'rem'
+        if(doodlerBottomSpace > startPoint + 30)
+            fall()
+    }, 30)
+}
+
+const fall = () => {
+    clearInterval(upTimerId)
+    isJumping = false
+    downTimerId = setInterval(() => {
+        doodlerBottomSpace -= .5
+        beardedDoodlerElement.style.bottom = doodlerBottomSpace + 'rem'
+        if(doodlerBottomSpace <= 0)     gameOver()
+        platforms.forEach(platform => {
+            if(
+                (doodlerBottomSpace >= platform.bottom) &&
+                (doodlerBottomSpace <= (platform.bottom + 1)) &&
+                ((doodlerLeftSpace + 6) >= platform.left) &&
+                (doodlerLeftSpace <= (platform.left + 9)) &&
+                !isJumping
+            ) {
+                console.log('landed')
+                startPoint = doodlerBottomSpace
+                jump()
+            }
+        })
+    }, 30)
+}
+
+function gameOver(){
+    console.log('Game is Over')
+    isGameOver = true
+    clearInterval(upTimerId)
+    clearInterval(downTimerId)
+}
+
+const control = (e) => {
+    if(e.key === 'ArrowLeft'){
+        moveLeft()
+    }else if(e.key === "ArrowRight"){
+        //moveRight
+    }else if(e.key === "ArrowUp"){
+        //moveUp
+    }
+}
+
+const moveLeft = () => {
+    isGoingLeft = true
+    leftTimerId = setInterval( function () {
+        if(doodlerLeftSpace >= 0){
+            doodlerLeftSpace -= .5
+            beardedDoodlerElement.style.left = doodlerLeftSpace + 'rem'
+        }
+    }, 30)
+}
+
+const start = () => {
+    if(!isGameOver){
+        createPlatforms()
+        createDoodler()
+        setInterval(movePlatforms, 30)
+        jump()
+        document.addEventListener('keyup', control)
+    }
+}
+
+
 
 //attach it to a button
 start()
